6fe72b4c7bfac4ed03637a66b5bd1aa97b468433
Rodrigo Prestes <rodrigo.prestes@4linux.com.br>
2022-09-05 19:10:18 -0300

    first commit

---
+++ pdown.py
@@ -0,0 +1,31 @@
+
+
+"""
+Simple script to download and write files in Python
+written for IBM Python & Data Science course
+Rodrigo feb 27, 2021
+"""
+
+import requests
+import os
+import sys
+
+class Pget(object):
+    #constructor
+    def __init__(self, url):
+        self.url = url
+        try:
+            r = requests.get(url)
+            file=r.url.split('/')[-1]
+            path=os.path.join(os.getcwd(),file)
+            with open(path, 'wb') as f:
+                f.write(r.content)
+                print(file,"was successfully downloaded.")
+        except:
+            print("Unable to reach the download the file!")
+
+if len(sys.argv) == 2:
+    link=sys.argv[1]
+    Pget(link)
+else:
+    print("pget.py by Solaris\nSyntax: python pget.py URL")
